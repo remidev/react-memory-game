01d98a612fc49a6994f4e2fcb7008b4a9082c885
remidev <28935481+remidev@users.noreply.github.com>
2019-03-25 21:32:08 -0400

    components

---
+++ src/components/Game/index.js
@@ -0,0 +1,106 @@
+// abstract game component, 'parent' of all other components
+import React, { Component } from "react";
+import Nav from "../Nav";
+import Header from "../Header";
+import Container from "../Container";
+import ClickItem from "../ClickItem";
+import Footer from "../Footer";
+import data from "../../data.json";
+
+// create game props
+class Game extends Component {
+  state = {
+    data,
+    score: 0,
+    topScore: 0
+  };
+
+  componentDidMount() {
+    this.setState({ data: this.shuffleData(this.state.data) });
+  }
+
+  handleCorrectGuess = newData => {
+    const { topScore, score } = this.state;
+    const newScore = score + 1;
+    const newTopScore = Math.max(newScore, topScore);
+
+    this.setState({
+      data: this.shuffleData(newData),
+      score: newScore,
+      topScore: newTopScore
+    });
+  };
+
+  handleIncorrectGuess = data => {
+    this.setState({
+      data: this.resetData(data),
+      score: 0
+    });
+  };
+
+  resetData = data => {
+    const resetData = data.map(item => ({ ...item, clicked: false }));
+    return this.shuffleData(resetData);
+  };
+
+  shuffleData = data => {
+    let i = data.length - 1;
+    while (i > 0) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = data[i];
+      data[i] = data[j];
+      data[j] = temp;
+      i--;
+    }
+    return data;
+  };
+
+  // click handler
+  handleItemClick = id => {
+    // pass ClickItem id
+    let guessedCorrectly = false;
+    // assume incorrect guess
+    const newData = this.state.data.map(item => {
+      // create new data by mapping over game state data
+      // to create new items for each 'card' in data.json
+      const newItem = { ...item };
+      if (newItem.id === id) {
+        // when mapping card with same id as initial event...
+        if (!newItem.clicked) {
+          // if this is first click since loss
+          newItem.clicked = true;
+          guessedCorrectly = true;
+        }
+      }
+      return newItem;
+    });
+    guessedCorrectly
+      ? this.handleCorrectGuess(newData)
+      : this.handleIncorrectGuess(newData);
+  };
+
+  // render call from top level component
+  render() {
+    return (
+      <div>
+        <Nav score={this.state.score} topScore={this.state.topScore} />
+        <Header />
+        <Container>
+          {this.state.data.map(item => (
+            <ClickItem
+              key={item.id}
+              id={item.id}
+              shake={!this.state.score && this.state.topScore}
+              handleClick={this.handleItemClick}
+              image={item.image}
+            />
+          ))}
+        </Container>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+// export game component
+export default Game;
